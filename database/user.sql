3b834b8cb5e6416a4a8bafbceec92f914bd4f175
Pujon Das Auvi <auvipujondas@gmail.com>
2024-04-24 22:28:57 +0600

    simple users api endpoint done with node js and mysql

--- database/user.sql
+++ database/user.sql
@@ -1,32 +1,18 @@
+CREATE DATABASE IF NOT EXISTS node_mysql;
+
 USE node_mysql;
 
-CREATE TABLE users (
+-- Create the users table
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE
 );
 
-/*DROP TABLE users*/
+ALTER TABLE users AUTO_INCREMENT = 1;
 
--- Create a trigger to adjust the auto-increment value
-DELIMITER //
-CREATE TRIGGER adjust_auto_increment
-BEFORE INSERT ON users
-FOR EACH ROW
-BEGIN
-    DECLARE max_id INT;
+-- Insert some initial data into the users table
+INSERT INTO users (name, email) VALUES ('pujon', 'pujonddd1d@gmails.com');
 
-    IF NEW.id IS NULL THEN
-        -- Get the maximum id value from the table
-        SELECT MAX(id) INTO max_id FROM users;
-
-        -- Increment the auto-increment value only if the insertion is successful
-        IF max_id IS NULL THEN
-            SET NEW.id = 1;
-        ELSE
-            SET NEW.id = max_id + 1;
-        END IF;
-    END IF;
-END;
-//
-DELIMITER ;
+-- Check the contents of the users table
+SELECT * FROM users;
